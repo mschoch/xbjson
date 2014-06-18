fa3e1fb8b17b159ab5ec1ff27b58419e8c85a2f8
Marty Schoch <marty.schoch@gmail.com>
2014-06-18 11:26:34 -0400

    updated to send HTTP posts

--- main.go
+++ main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"text/template"
 	"time"
 )
 
 var cacheDuration = flag.Duration("cacheDuration", 5*time.Minute, "expire device revs after this duration")
+var urlTepmlateString = flag.String("url", "http://localhost:4793/{{.d}}?rev={{.n}}", "url to post message to")
+var removeDevice = flag.Bool("removeDevice", true, "remove device from message")
+var removeRev = flag.Bool("removeRev", true, "remove rev from message")
+var addTimestamp = flag.Bool("addTimestamp", true, "add timestamp received to message")
 
 func main() {
 
 	flag.Parse()
+
+	urlTemplate, err := template.New("url").Parse(*urlTepmlateString)
+	if err != nil {
+		log.Fatalf("error parsing url template: %v", err)
+	}
 
 	cache := NewTimeRevCache(*cacheDuration)
 
@@ -59,9 +71,31 @@ func main() {
 
 		ok = cache.CheckAndUpdate(device, int(rev))
 		if ok {
-			log.Printf("new message from %s at %d", device, int(rev))
-		} else {
-			log.Printf("duplicate message from %s at %d", device, int(rev))
+			buffer := new(bytes.Buffer)
+			urlTemplate.Execute(buffer, jsonData)
+			if *removeDevice {
+				delete(jsonData, "d")
+			}
+			if *removeRev {
+				delete(jsonData, "n")
+			}
+			if *addTimestamp {
+				jsonData["ts"] = time.Now().Format(time.RFC3339)
+			}
+
+			jsonOut, err := json.Marshal(jsonData)
+			if err != nil {
+				log.Printf("error marshaling JSON: %v", err)
+				continue
+			}
+			jsonBuffer := bytes.NewBuffer(jsonOut)
+			resp, err := http.Post(buffer.String(), "application/json", jsonBuffer)
+			if err != nil {
+				log.Printf("error http post: %v", err)
+				continue
+			}
+			log.Printf("sent message: %s, to %s, got %s", jsonOut, buffer.String(), resp.Status)
 		}
+
 	}
 }
